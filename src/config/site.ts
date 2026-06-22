d8bd20100e31c6c89ba41fcf921723ac34328862
Piotr Herda <bogusia@permanentnybielsko.com>
2026-06-22 14:58:40 +0200

    Add initial homepage layout

---
+++ src/config/site.ts
@@ -0,0 +1,35 @@
+export const siteConfig = {
+	name: 'Inwestycja w Myjnię',
+	shortName: 'Inwestycja w Myjnię',
+	description:
+		'Praktyczny poradnik dla inwestora myjni bezdotykowej: koszty budowy, opłacalność, działka, formalności, technologia i wybór sprawdzonego rozwiązania.',
+	url: 'https://inwestycjawmyjnie.pl',
+	email: 'analiza@inwestycjawmyjnie.pl',
+	indexingEnabled: false,
+	nav: [
+		{
+			label: 'Koszty',
+			href: '#koszty'
+		},
+		{
+			label: 'Opłacalność',
+			href: '#oplacalnosc'
+		},
+		{
+			label: 'Działka',
+			href: '#dzialka'
+		},
+		{
+			label: 'Technologia',
+			href: '#technologia'
+		},
+		{
+			label: 'Poradnik',
+			href: '#poradnik'
+		},
+		{
+			label: 'Kontakt',
+			href: '#kontakt'
+		}
+	]
+};
